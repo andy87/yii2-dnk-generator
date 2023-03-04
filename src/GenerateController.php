e181537b9848dab4b93f17a5287a0b9f660a9d1d
Andrey and_y87 Kidin <kidin_a@list.ru>
2023-03-04 17:07:59 +0300

    fix

--- src/GenerateController.php
+++ src/GenerateController.php
@@ -224,12 +224,12 @@ class GenerateController extends Controller
         $root = $this->getTemplatePath();
 
         $this->copyDirectoryStructure(
-            "$root/source/common/components/interfaces/",
+            "/common/components/interfaces/",
             Yii::getAlias('@common/components/interfaces/')
         );
 
         $this->copyDirectoryStructure(
-            "$root/source/common/components/resources/",
+            "/common/components/resources/",
             Yii::getAlias('@common/components/resources/')
         );
 
